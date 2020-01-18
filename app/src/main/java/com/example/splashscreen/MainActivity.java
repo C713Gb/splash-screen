817e3a44050c5fdb8a3a20a34f54a54c3ae0ee19
GouravBanerjee <banerjeec713@gmail.com>
2020-01-18 19:51:31 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int WELCOME_TIMEOUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(welcome);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, WELCOME_TIMEOUT);
+
+    }
+}
